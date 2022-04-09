96ed1ef1f79e9ea231860a7ffddf6a39c56d82aa
im4kshay <akshaysayre7@gmail.com>
2022-04-10 02:02:54 +0530

    Given 3 Strings find the maximum

--- FindMaximumProblem/Program.cs
+++ FindMaximumProblem/Program.cs
@@ -17,16 +17,16 @@ namespace FindMaximumProblem
             MaximumNumberCheck maximumNumberCheck = new MaximumNumberCheck();
 
             //Maximum Number At First Place
-            double outputFloat1 = maximumNumberCheck.MaximumFloatNumber(14.1, 2.82, 7.33);
-            Console.WriteLine("Maximum Number of Float at First Place : " + outputFloat1);
+            string outputString1 = maximumNumberCheck.MaximumStringNumber("Peach", "Apple", "Banana");
+            Console.WriteLine("Maximum Number of String at First Place : " + outputString1);
 
             //Maximum Number At Second Place
-            double outputFloat2 = maximumNumberCheck.MaximumFloatNumber(4.1, 28.2, 12.8);
-            Console.WriteLine("Maximum Number of Integer at Second Place : " + outputFloat2);
+            string outputString2 = maximumNumberCheck.MaximumStringNumber("Apple", "Peach", "Banana");
+            Console.WriteLine("Maximum Number of String at Seond Place : " + outputString2);
 
             //Maximum Number At Third Place
-            double outputFloat3 = maximumNumberCheck.MaximumFloatNumber(75.9, 68.47, 77.3);
-            Console.WriteLine("Maximum Number of Integer at Third Place : " + outputFloat3);
+            string outputString3 = maximumNumberCheck.MaximumStringNumber("Apple", "Banana", "Peach");
+            Console.WriteLine("Maximum Number of String at Third Place : " + outputString3);
 
             Console.ReadLine();
         }
