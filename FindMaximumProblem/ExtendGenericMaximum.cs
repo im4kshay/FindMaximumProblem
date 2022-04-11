4f1c6c8faae7ac63fb78be8cc415c5147140d37d
im4kshay <akshaysayre7@gmail.com>
2022-04-11 11:31:08 +0530

    Extend the max method to also print the max to std out using Generic Method

--- FindMaximumProblem/ExtendGenericMaximum.cs
+++ FindMaximumProblem/ExtendGenericMaximum.cs
@@ -8,6 +8,7 @@ namespace FindMaximumProblem
 {
     class ExtendGenericMaximum<T> where T : IComparable
     {
+        //Instance Variable Generic 
         public T[] value;
 
         //Parameter Constructor
@@ -16,21 +17,33 @@ namespace FindMaximumProblem
             this.value = value;
         }
 
+        //Taking Values and Make Sort
         public T[] Sort(T[] values)
         {
             Array.Sort(values);
             return values;
         }
 
+        //Taking Values and Make Sort
         public T MaxValue(params T[] values)
         {
             var sorted_values = Sort(values);
             return sorted_values[sorted_values.Length-1];
         }
+
+        //Returning Max Value
         public T MaxMethod()
         {
             var max = MaxValue(this.value);
             return max;
+        }
+
+        //To Print The Value
+        internal void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum Value is : " + max);
+
         }
 
     }
--- FindMaximumProblem/Program.cs
+++ FindMaximumProblem/Program.cs
@@ -11,27 +11,22 @@ namespace FindMaximumProblem
         static void Main(string[] args)
         {
             //Welcome Message
-            Console.WriteLine("Welcome to Maximum Number");
+            Console.WriteLine("=-=-=-=-=-Welcome to the Find Maximum Problem using Generics-=-=-=-=-=");
 
+            //Array With Different Data Types
             int[] intArray = { 34, 23, 12, 36 };
             double[] doubleArray = { 12.5, 45.65, 987.45, 658.78 };
             string[] stringArray = { "Apple", "Peach", "Banana" };
 
-            // Passing integer input with max number at first place
+            // Objects 
             ExtendGenericMaximum<int> extendGenericMaximum = new ExtendGenericMaximum<int>(intArray);
-            int outputInt = extendGenericMaximum.MaxMethod();
-
             ExtendGenericMaximum<double> extendGenericMaximum1 = new ExtendGenericMaximum<double>(doubleArray);
-            double outputDouble = extendGenericMaximum1.MaxMethod();
-
             ExtendGenericMaximum<string> extendGenericMaximum2 = new ExtendGenericMaximum<string>(stringArray);
-            string outputString = extendGenericMaximum2.MaxMethod();
-
 
             //Display Output
-            Console.WriteLine("Integer Max : " + outputInt);
-            Console.WriteLine("Float Max : " + outputDouble);
-            Console.WriteLine("String Max : " + outputString);
+            extendGenericMaximum.PrintMaxValue();
+            extendGenericMaximum1.PrintMaxValue();
+            extendGenericMaximum2.PrintMaxValue();
 
             Console.ReadLine();
 
